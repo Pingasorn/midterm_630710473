0aad9b3c82410779af951f1e966eefa9172d2b7e
Pingasorn <Pattarapanpattarapan@gmail.com>
2023-09-03 11:59:10 +0700

    Secon commit

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -53,12 +53,175 @@ class _HomePageState extends State<HomePage> {
   }
 
   _buildQuizView() {
-    // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+    return Container(
+      width: 200,
+      height: 400,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20), // Reduce the border radius
+        border: Border.all(
+          color: Colors.black,
+          width: 3.0,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                padding: EdgeInsets.all(8.0), // Adjust the padding as needed
+                child: Text(
+                  'Question 1 of 30',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 15.0),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 16.0), // Add spacing
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 110, 184, 151), // สีพื้นหลัง
+              border: Border.all(
+                color: Colors.black,
+                width: 1.0, // ปรับความหนาขอบเส้นตามต้องการ
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(8.0), // ปรับรูปร่างขอบเส้นตามต้องการ
+              ),
+            ),
+            padding:
+                EdgeInsets.all(8.0), // ปรับการเว้นระยะห่างของข้อความตามต้องการ
+            child: Text(
+              'What is the capition of France ?',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black, fontSize: 15.0),
+            ),
+          ),
+          SizedBox(height: 16.0), // Add spacing
+          // You can add an answer input field here
+          SizedBox(height: 16.0), // Add spacing
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1.0, // Adjust the border width as needed
+              ),
+              borderRadius: BorderRadius.all(
+                  Radius.circular(8.0)), // Adjust the border radius as needed
+            ),
+            padding: EdgeInsets.all(8.0), // Adjust the padding as needed
+            child: Text(
+              '1️⃣ Berlin                                               ',
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.black, fontSize: 15.0),
+            ),
+          ),
+          SizedBox(height: 16.0), // Add spacing
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1.0, // Adjust the border width as needed
+              ),
+              borderRadius: BorderRadius.all(
+                  Radius.circular(8.0)), // Adjust the border radius as needed
+            ),
+            padding: EdgeInsets.all(8.0), // Adjust the padding as needed
+            child: Text(
+              '2️⃣ London                                           ',
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.black, fontSize: 15.0),
+            ),
+          ),
+          SizedBox(height: 16.0), // Add spacing
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1.0, // Adjust the border width as needed
+              ),
+              borderRadius: BorderRadius.all(
+                  Radius.circular(8.0)), // Adjust the border radius as needed
+            ),
+            padding: EdgeInsets.all(8.0), // Adjust the padding as needed
+            child: Text(
+              '3️⃣ Madrid                                            ',
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.black, fontSize: 15.0),
+            ),
+          ),
+          SizedBox(height: 16.0), // Add spacing
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1.0, // Adjust the border width as needed
+              ),
+              borderRadius: BorderRadius.all(
+                  Radius.circular(8.0)), // Adjust the border radius as needed
+            ),
+            padding: EdgeInsets.all(8.0), // Adjust the padding as needed
+            child: Text(
+              '4️⃣ Paris                                           ✔️',
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.black, fontSize: 15.0),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   _buildButtonPanel() {
     // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              side: BorderSide(color: Colors.black),
+              backgroundColor: Colors.redAccent,
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 60.0),
+              child: Text(
+                '<',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              side: BorderSide(color: Colors.black),
+              backgroundColor: Colors.greenAccent,
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 60.0),
+              child: Text(
+                '>',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
